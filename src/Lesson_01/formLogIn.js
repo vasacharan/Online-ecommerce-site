711866d0b399dd736d672d4d4062a4e613d7b7ed
charanpratap <vasacp193691@gmail.com>
2023-06-12 17:05:26 +0530

    some changes

--- src/Lesson_01/formLogIn.js
+++ src/Lesson_01/formLogIn.js
@@ -1,18 +1,2 @@
-import React from 'react'
-
-const formLogIn = () => {
-  
-  return (
-    <div>
-      <center>
-      <h5>Java script</h5>
-       <form>
-          <input type='text' placeholder='Username' /><br/>
-          <input type='password' placeholder='Password' />
-       </form> 
-      </center>   
-    </div>
-  )
-}
-
-export default formLogIn;
+let x = document.getElementsByTagName("span");
+console.log(x);
